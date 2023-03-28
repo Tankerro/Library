9a4cde9a8edbec0244b4c15502089762b7ef7073
Vladimir <bulychev.v.m@gmail.com>
2023-03-28 10:37:15 +0700

    Library

---
+++ Assets/Assets/Scripts/Character_controller.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_controller : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    public float speed = 12f;
+    public float gravity = -9.8f;
+
+    public float jumpHeight = 0.5f; 
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    public bool isGrounded;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        characterController.Move(move * speed);
+
+        if(isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
